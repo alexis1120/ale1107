63f91bee9cc132b03f2e078d340c804f38ce406f
ale <alexisacostarodriguez@hotmail.com>
2025-02-16 03:44:36 -0500

    unisat
    {"p":"brc-20","op":"deploy","tick":"Ahfdt","lim":"1000","max":"21000000","self_mint":"true"}

---
+++ import re.py
@@ -0,0 +1,16 @@
+import re
+
+bitcoin_address = "bc1qj8fynf8hfd5333n42guyfxdnv30gv8wpntz3qf"
+def extract_bitcoin_address(text):
+    pattern = r'\b(bc1[ac-hj-np-z02-9]{25,39})\b'
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1)
+    return match.group(1), match.span()
+
+text = "bc1qj8fynf8hfd5333n42guyfxdnv30gv8wpntz3qf"
+address, span = extract_bitcoin_addrestext = "bc1qj8fynf8hfd5333n42guyfxdnv30gv8wpntz3qf"
+
+address, span = extract_bitcoin_address(text)
+print(f"Extracted Bitcoin Address: {address}")
+print(f"Span: {span}")
